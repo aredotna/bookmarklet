b9ca0e26d78abbf3179a42dcc4231ac4efdfded0
Dan Brewster <dnbrwstr@gmail.com>
2013-05-01 10:57:00 -0400

    Remove old loader

--- app/loader.js
+++
@@ -1,4 +0,0 @@
-var script = document.createElement('script');
-script.src = "http://localhost:3333/javascripts/marklet.js";
-body = document.getElementsByTagName('body')[0];
-body.appendChild(script);
